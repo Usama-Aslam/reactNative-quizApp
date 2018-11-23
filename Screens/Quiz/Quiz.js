4dc4345b7ace0b0792351074d9fca8258ccac4b6
Usama-Aslam <uaslam345@gmail.com>
2018-11-23 20:38:00 +0500

    radio button and its value is bind with updateStatus function

--- Screens/Quiz/Quiz.js
+++ Screens/Quiz/Quiz.js
@@ -13,23 +13,66 @@ class Quiz extends React.Component {
         super(props)
         const { question } = this.props.navigation.state.params
         this.state = {
-            Question: question[0].question,
-            opt1: question[0].incorrect_answers[0],
-            opt2: question[0].incorrect_answers[1],
-            opt3: question[0].incorrect_answers[2],
-            opt4: question[0].correct_answer,
-
-            checked: [false, false, false, false]
+            obj: {
+                Question: question[0].question,
+                opt1: question[0].incorrect_answers[0],
+                opt2: question[0].incorrect_answers[1],
+                opt3: question[0].incorrect_answers[2],
+                opt4: question[0].correct_answer,
+            },
+            load: 0,
+            correct: null,
+            checked: false
         }
     }
 
 
+    next = () => {
+        const { Question, opt1, opt2, opt3, opt4 } = this.setState.obj
+        const { question } = this.props.navigation.state.params
+        // if (question.correct_answer.equals())
+    }
+
+    updateStatus = (e) => {
+        let { checked } = this.state;
+
+        switch (e) {
+            case 1: {
+                checked = 1
+                break;
+            }
+            case 2: {
+                checked = 2
+                break;
+            }
+            case 3: {
+                checked = 3
+                break;
+            }
+            case 4: {
+                checked = 4
+                break;
+            }
+            default: {
+                console.log("please select one")
+            }
+
+        }
+
+        this.setState({
+            checked
+        })
+
+    }
+
     render() {
         const { question } = this.props.navigation.state.params;
-        const { Question, opt1, opt2, opt3, opt4, checked } = this.state
+        let { Question, opt1, opt2, opt3, opt4 } = this.state.obj
+        console.log("opt statuss", this.state.checked)
         return (
             <View style={{ flex: 1 }}>
                 <Text>{Question}</Text>
+
                 <Container>
                     <Content>
                         <ListItem>
@@ -37,35 +80,59 @@ class Quiz extends React.Component {
                                 <Text>{opt1}</Text>
                             </Left>
                             <Right>
-                                <Radio selected={checked[0]} />
+                                <Radio
+                                    selected={this.state.checked == 1}
+                                    onPress={() => {
+                                        this.updateStatus(1)
+                                    }}
+                                />
                             </Right>
                         </ListItem>
+
                         <ListItem>
                             <Left>
                                 <Text>{opt2}</Text>
                             </Left>
                             <Right>
-                                <Radio selected={checked[0]} />
+                                <Radio
+                                    selected={this.state.checked == 2}
+                                    onPress={() => {
+                                        this.updateStatus(2)
+                                    }}
+                                />
                             </Right>
                         </ListItem>
+
                         <ListItem>
                             <Left>
                                 <Text>{opt3}</Text>
                             </Left>
                             <Right>
-                                <Radio selected={checked[0]} />
+                                <Radio
+                                    selected={this.state.checked == 3}
+                                    onPress={() => {
+                                        this.updateStatus(3)
+                                    }}
+                                />
                             </Right>
                         </ListItem>
+
                         <ListItem>
                             <Left>
                                 <Text>{opt4}</Text>
                             </Left>
                             <Right>
-                                <Radio selected={checked[0]} />
+                                <Radio
+                                    selected={this.state.checked == 4}
+                                    onPress={() => {
+                                        this.updateStatus(4)
+                                    }}
+                                />
                             </Right>
                         </ListItem>
                     </Content>
                 </Container>
+
                 {/* <Button
 
                     title="Start"
